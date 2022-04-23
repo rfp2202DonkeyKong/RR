b67d923f11f28cb7b49e00bd56b4c343fe4cee58
Sean McNamara <seanmcnamara33@gmail.com>
2022-04-22 21:16:17 -0700

    fully complete functionality for both get /reviews and get/revews/meta, as well as put /reviews/helpful and put /reviews/report. Post request to /reviews is nearly complete as well

--- Database/database.js
+++ Database/database.js
@@ -38,14 +38,64 @@ const getReviews = async (productId, sort) => {
   }
 }
 
-const getMetaReviews = (productId) => {
+const getMetaReviews = async (productId) => {
   try {
-
+    let client = await pool.connect();
+    await client.query(`CREATE TABLE current_product AS SELECT * FROM reviews WHERE product_id = ${productId}`);
+    let results = await client.query(
+      `SELECT
+        characteristicreviews.id AS id,
+        current_product.id AS review_id,
+        characteristicreviews.characteristic_id AS char_id,
+        rating,
+        recommend,
+        name,
+        value
+      FROM current_product
+      INNER JOIN characteristicreviews ON current_product.id = characteristicreviews.review_id
+      INNER JOIN characteristics ON characteristicreviews.characteristic_id = characteristics.id;`);
+      client.query(`DROP TABLE current_product`);
+      client.release();
+      return results.rows;
   } catch(err) {
     console.log(`Error found in getMetaReviews: ${err}`)
   }
 }
 
+const reportReview = async (review_id) => {
+  try {
+    let client = await pool.connect();
+    await client.query(`UPDATE reviews SET reported = true WHERE reviews.id = ${review_id}`)
+    client.release();
+  } catch(err) {
+    console.log(`Error found in reportReview: ${err}`);
+  }
+};
+
+const addHelpfulReview = async (review_id) => {
+  try {
+    let client = await pool.connect();
+    await client.query(`UPDATE reviews SET helpfulness = helpfulness + 1 WHERE reviews.id = ${review_id}`)
+    client.release();
+  } catch(err) {
+    console.log(`Error found in addHelpfulReview: ${err}`);
+  }
+};
+
+const postReview = async (post) => {
+  let date = Date.now()
+  try {
+    let client = await pool.connect();
+    await client.query(`INSERT INTO reviews(product_id, rating, date, summary, body, recommend, reported, reviewer_name, reviewer_email, response, helpfulness) VALUES (${post.product_id}, ${post.rating}, ${date}, ${post.summary}, ${post.body}, ${post.recommend}, false, ${post.name}, ${post.email}, ${post.photos}, ${post.characteristics})`);
+    client.release();
+  } catch(err) {
+    console.log(`Error found in postReview: ${err}`);
+  }
+};
+
 module.exports.getReviews = getReviews;
 module.exports.getMetaReviews = getMetaReviews;
+module.exports.reportReview = reportReview;
+module.exports.addHelpfulReview = addHelpfulReview;
+module.exports.postReview = postReview;
 
--- Server/index.js
+++ Server/index.js
@@ -1,25 +1,165 @@
-const {getReviews} = require('../Database/database.js');
+const {getReviews, getMetaReviews, reportReview, addHelpfulReview, postReview} = require('../Database/database.js');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
+const getCharacteristicData = (data) => {
+  let characteristics = new Set();
+  let chars = [];
+  var charsObj = {};
+  var FitCounter = 0;
+  var FitValue = 0;
+  var FitId = 0;
+  var QualityCounter = 0;
+  var QualityValue = 0;
+  var QualityId = 0;
+  var LengthCounter = 0;
+  var LengthValue = 0;
+  var LengthId = 0;
+  var ComfortCounter = 0;
+  var ComfortValue = 0;
+  var ComfortId = 0;
+  var WidthCounter = 0;
+  var WidthValue = 0;
+  var WidthId = 0;
+  var SizeCounter = 0;
+  var SizeValue = 0;
+  var SizeId = 0;
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].name === 'Fit') {
+      FitCounter++;
+      FitValue += data[i].value;
+      FitId = data[i].char_id;
+    }
+    if (data[i].name === 'Quality') {
+      QualityCounter++;
+      QualityValue += data[i].value;
+      QualityId = data[i].char_id;
+    }
+    if (data[i].name === 'Size') {
+      SizeCounter++;
+      SizeValue += data[i].value;
+      SizeId = data[i].char_id;
+    }
+    if (data[i].name === 'Comfort') {
+      ComfortCounter++;
+      ComfortValue += data[i].value;
+      ComfortId = data[i].char_id;
+    }
+    if (data[i].name === 'Length') {
+      LengthCounter++;
+      LengthValue += data[i].value;
+      LengthId = data[i].char_id;
+    }
+    if (data[i].name === 'Width') {
+      WidthCounter++;
+      WidthValue += data[i].value;
+      WidthId = data[i].char_id;
+    }
+    characteristics.add(data[i].name);
+  }
+  var characteristicsArray = Array.from(characteristics);
+  for (var j = 0; j < characteristicsArray.length; j++) {
+    if (characteristicsArray[j] === 'Fit') {
+      charsObj['Fit'] = createCharacteristicObject((FitValue / FitCounter), FitId);
+    }
+    if (characteristicsArray[j] === 'Quality') {
+      charsObj['Quality'] = createCharacteristicObject((QualityValue / QualityCounter), QualityId)
+    }
+    if (characteristicsArray[j] === 'Size') {
+      charsObj['Size'] = createCharacteristicObject((SizeValue / SizeCounter), SizeId)
+    }
+    if (characteristicsArray[j] === 'Comfort') {
+      charsObj['Comfort'] = createCharacteristicObject((ComfortValue / ComfortCounter), ComfortId)
+    }
+    if (characteristicsArray[j] === 'Length') {
+      charsObj['Length'] = createCharacteristicObject((LengthValue / LengthCounter), LengthId);
+    }
+    if (characteristicsArray[j] === 'Width') {
+      charsObj['Width'] = createCharacteristicObject((WidthValue / WidthCounter), WidthId)
+    }
+  }
+  return charsObj;
+};
+
+const createCharacteristicObject = (value, id) => {
+  var innerCharacteristic = {
+    id: id,
+    value: value.toString()
+  };
+  return innerCharacteristic;
+}
+
+const getRatingsData = (data) => {
+  var ratingsObject = {
+    '1': 0,
+    '2': 0,
+    '3': 0,
+    '4': 0,
+    '5': 0
+  }
+  for (var i = 0; i < data.length; i++) {
+    if (i === 0) {
+      ratingsObject[data[i].rating]++;
+    }
+    if (i > 0 && data[i].review_id !== data[i - 1].review_id) {
+      ratingsObject[data[i].rating]++;
+    }
+  }
+  for (var rating in ratingsObject) {
+    ratingsObject[rating] = ratingsObject[rating].toString();
+  }
+  return ratingsObject;
+};
+
+const getRecommendedData = (data) => {
+  var recommendationObject = {
+    'false': 0,
+    'true': 0
+  }
+  for (var i = 0; i < data.length; i++) {
+    if (i === 0) {
+      if (data[i].recommend === true) {
+        recommendationObject.true++;
+      } else {
+        recommendationObject.false++;
+      }
+    }
+    if (i > 0 && data[i].review_id !== data[i - 1].review_id) {
+      if (data[i].recommend === true) {
+        recommendationObject.true++;
+      } else {
+        recommendationObject.false++;
+      }
+    }
+  }
+  recommendationObject.true = recommendationObject.true.toString();
+  recommendationObject.false = recommendationObject.false.toString();
+  return recommendationObject;
+}
 //works
-app.get('/reviews/meta', (req, res) => {
-  console.log(req.query.product_id)
-  res.send('this is the meta response');
+app.get('/reviews/meta', async (req, res) => {
+  var metaReviews = await getMetaReviews(req.query.product_id);
+  var result = {
+    product_id: req.query.product_id.toString(),
+    ratings: getRatingsData(metaReviews),
+    recommended: getRecommendedData(metaReviews),
+    characteristics: getCharacteristicData(metaReviews)
+  }
+  console.log(result);
+  res.send(result);
 })
 
 //works
 app.get('/reviews', async (req, res) => {
   let sort = req.query.sort;
   if (sort === 'helpful') {
-    console.log('why you no work')
     sort = 'helpfulness DESC'
   } else if (sort === 'newest') {
     sort = 'date DESC'
   } else if (sort === 'relevance') {
-    sort = 'helpfulness * date DESC'
+    sort = 'CASE WHEN helpfulness > 0 THEN helpfulness * date ELSE date END DESC'
   } else {
     sort = 'review_id DESC'
   }
@@ -36,8 +176,8 @@ app.get('/reviews', async (req, res) => {
       formattedReviews[currentReviewPointer - 1].photos.push(currentPhoto);
     } else {
       let currentReview = {
-        review_id: reviews[i].review_id,
-        rating: reviews[i].rating,
+        review_id: Number(reviews[i].review_id),
+        rating: Number(reviews[i].rating),
         summary: reviews[i].summary,
         recommend: reviews[i].recommend,
         response: reviews[i].response,
@@ -68,22 +208,26 @@ app.get('/reviews', async (req, res) => {
 })
 
 //works
-app.post('/reviews', (req, res) => {
+app.put('/reviews/:review_id/report', async (req, res) => {
+  await reportReview(req.params.review_id);
+  res.sendStatus(204);
+})
+
+//works
+app.put('/reviews/:review_id/helpful', async (req, res) => {
+  console.log(req.params.review_id)
+  await addHelpfulReview(req.params.review_id);
+  res.sendStatus(204);
+})
+
+//works
+app.post('/reviews', async (req, res) => {
   console.log(req.body);
+  await postReview(req.body);
   res.send('Hopefully this works');
 })
 
-//unchecked
-app.put('/reviews/:review_id/helpful', (req, res) => {
-  console.log('helpful');
-  res.send('helpful');
-})
 
-//unchecked
-app.put('/reviews/:review_id/report', (req, res) => {
-  console.log('report');
-  res.send('report');
-})
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
